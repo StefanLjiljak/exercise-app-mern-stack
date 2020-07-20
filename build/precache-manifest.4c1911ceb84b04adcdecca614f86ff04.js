22473de67fb6a6355fe19e2d338b9eee39aa0381
StefanLjiljak <stefanljiljak@gmail.com>
2020-07-20 21:57:06 +0200

    first commit

---
+++ build/precache-manifest.4c1911ceb84b04adcdecca614f86ff04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7750e99749b51020809cc935abc594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88a57df6da0b362c7c44",
+    "url": "/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "88a57df6da0b362c7c44",
+    "url": "/static/js/2.e7ea6630.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.e7ea6630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790c8225d1068f3e92e8",
+    "url": "/static/js/main.0d656bff.chunk.js"
+  },
+  {
+    "revision": "666df64c05a4cb398c37",
+    "url": "/static/js/runtime-main.d66d7b0a.js"
+  }
+]);
